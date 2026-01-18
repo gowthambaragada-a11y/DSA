86b2b4d7fcdbf2c8e76e2fe25ad12893b0121798
Gowtham sai ram Baragada <gowthambaragada@gmail.com>
2026-01-18 09:25:39 +0530

    Time: 0 ms (100%), Space: 42.5 MB (11.17%) - LeetHub

--- 0233-number-of-digit-one/0233-number-of-digit-one.java
+++ 0233-number-of-digit-one/0233-number-of-digit-one.java
@@ -1,17 +1,13 @@
 class Solution {
     public int countDigitOne(int n) {
-        int c=0,a=n;
-        if(n==3184191)return 2978524;
-        if(n==824883294)return 767944060;
-        if(n==999999999)return 900000000;
-        if(n==0)return 0;
-        if(n>=1&&n<10)return 1;
-        if(n==10)return 2;
-        while(a>0){
-            int b=a%10;
-            if(b==1)c++;
-            a/=10;
-        }
-        return c+countDigitOne(n-1);
+        if(n<=0)return 0;
+        String s=String.valueOf(n);
+        int len=s.length();
+        if(len==1)return n >= 1 ? 1 : 0;
+        int highest=s.charAt(0)-'0';
+        int pow=(int)Math.pow(10,len-1);
+        int rest=n-highest*pow;
+        if(highest==1)return countDigitOne(pow-1)+rest+1+countDigitOne(rest);
+        else return highest*countDigitOne(pow-1)+pow+countDigitOne(rest);
     }
 }
