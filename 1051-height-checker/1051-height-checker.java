670c17e04405290c687cb0aab0364447c6b4ac3e
Gowtham sai ram Baragada <gowthambaragada@gmail.com>
2026-01-25 20:19:01 +0530

    Time: 5 ms (34.88%), Space: 43.7 MB (8.38%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int []arr=new int[heights.length];
+        for(int i=0;i<heights.length;i++)arr[i]=heights[i];
+        Arrays.sort(heights);
+        int count=0;
+        for(int i=0;i<heights.length;i++){
+            if(arr[i]!=heights[i])count++;
+        }
+        return count;
+    }
+}
