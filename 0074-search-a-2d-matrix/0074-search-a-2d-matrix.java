12c97aecf6f0f71985ea1e2147101ead568ded4c
Gowtham sai ram <gowthambaragada@gmail.com>
2026-02-24 09:38:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i<matrix.length;i++){
+            if(matrix[i][matrix[i].length-1]==target)return true;
+            if(matrix[i][matrix[i].length-1]>target)return bs(matrix[i],target);
+        }
+        return false;
+    }
+    boolean bs(int []arr,int target){
+        int l=1,r=arr.length-1;
+        while(l<=r){
+            int m=l-(l-r)/2;
+            if(arr[m]==target)return true;
+            if(arr[m]<target)l=m+1;
+            if(arr[m]>target)r=m-1;
+        }
+        return false;
+    }
+}
