72ec7a03c73437a375a03976901b1e12a19f9188
Gowtham sai ram <gowthambaragada@gmail.com>
2026-02-24 10:31:36 +0530

    Time: 3 ms (4.74%), Space: 43.4 MB (82.02%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,11 +1,14 @@
 class Solution {
+    static int g[][];
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        g = new int[obstacleGrid.length][obstacleGrid[0].length];
         return path(obstacleGrid,0,0);
     }
     int path(int [][]grid,int i,int j){
         if(i>=grid.length||j>=grid[0].length)return 0;
         if(grid[i][j]==1)return 0;
+        if(g[i][j]!=0) return g[i][j];
         if(i==grid.length-1&&j==grid[i].length-1)return 1;
-        return path(grid,i+1,j)+path(grid,i,j+1);
+        return g[i][j] = path(grid,i+1,j)+path(grid,i,j+1);
     }
 }
