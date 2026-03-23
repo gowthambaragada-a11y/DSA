c219dff732e4f230a949a309121add1b2363a79e
Gowtham sai ram <gowthambaragada@gmail.com>
2026-03-23 20:00:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,10 +1,5 @@
 class Solution {
     public String defangIPaddr(String address) {
-        String r="";
-        for(char i:address.toCharArray()){
-            if(i=='.')r+="[.]";
-            else r+=i;
-        }
-        return r;
+        return address.replace(".","[.]");
     }
 }
